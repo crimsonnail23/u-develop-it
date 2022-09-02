813ab06aebf736a839d9161d9138d1eef1362bae
night <yseddiq@gmail.com>
2022-09-02 15:53:36 -0400

    added database, and table, also seeded said table

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+    ('ronald', 'firbank', 1),
+    ('Virgina', 'Wolf', 1),
+    ('Piers', 'Gaveston', 0),
+    ('Chuck', 'LeRoi', 1),
+    ('Katherine', 'Mansfield', 1),
+    ('Dora', 'Carrington', 0),
+    ('Edward', 'Bellamy', 0),
+    ('Montague', 'Summers', 1),
+    ('Octavia', 'Buttler', 1),
+    ('Unica', 'Zurn', 1);
