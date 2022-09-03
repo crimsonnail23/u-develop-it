fa0f7299a4531d9453f5708d39450fd8b9703656
night <yseddiq@gmail.com>
2022-09-03 16:03:40 -0400

    added voter routes

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+//connect to database.
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'election'
+    }
+);
+
+module.exports =db;
